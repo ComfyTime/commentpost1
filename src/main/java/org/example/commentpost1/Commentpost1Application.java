ea434ef2450d3c68026cf7e954b7801eeb0218bf
ComfyTime <yhjunphysics@gmail.com>
2025-02-24 18:41:41 +0900

    Initial commit

---
+++ src/main/java/org/example/commentpost1/Commentpost1Application.java
@@ -0,0 +1,13 @@
+package org.example.commentpost1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Commentpost1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Commentpost1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/commentpost1/Commentpost1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.commentpost1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Commentpost1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
